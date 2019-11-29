3f0d0bef62330583183678ce12ed794b3dbf57ad
sakusaku <saku.kago@gmail>
2019-11-29 17:34:36 +0900

    te

---
+++ Resources/player-idle-1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="player-idle-1" tilewidth="240" tileheight="240" tilecount="1" columns="1">
+ <image source="image/Sprites/player/player-idle/player-idle-1.png" width="240" height="240"/>
+</tileset>
